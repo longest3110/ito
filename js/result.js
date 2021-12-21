89e13f8b14bb766489a5aae7a80fe8cbb16d6444
macbook <longest3110@gmail.com>
2021-12-22 00:25:14 +0900

    [feature/結果] キーワードから、任意の数値を取得

--- js/result.js
+++ js/result.js
@@ -11,6 +11,11 @@ const result = {
 		const keyWord = getParam('k');
 		const playerNo = parseInt(getParam('p'), 10);
 
+		//キーワードから、任意の数値を取得
+		const seed = getStrToNum(keyWord);
+		
+		//1〜100までのランダムな数値が入った配列を作成
+
 	},
 
 	//関数部
@@ -28,6 +33,18 @@ function getParam(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+//文字列から任意の数値を取得
+function getStrToNum(keyWord) {
+	var result = 0;
+
+	for(var i = 0; i < keyWord.length; i++) {
+		result += parseInt(keyWord.charCodeAt(i));
+	}
+
+	//0対策のため、+1しておく
+	return result + 1;
 }
 
 // Vue.jsを初期化
