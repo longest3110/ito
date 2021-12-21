c3c5793c872152119185628a6c1e459b10de7dee
macbook <longest3110@gmail.com>
2021-12-22 00:15:56 +0900

    [feature/結果] パラメーター取得

--- js/result.js
+++ js/result.js
@@ -5,10 +5,29 @@ const result = {
 		}
 	},
 
+	//初期処理
+	mounted : function(){
+		//パラメーター取得
+		const keyWord = getParam('k');
+		const playerNo = parseInt(getParam('p'), 10);
+
+	},
+
 	//関数部
 	methods: {
 		
 	}
+}
+
+//パラメーター取得
+function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 // Vue.jsを初期化
